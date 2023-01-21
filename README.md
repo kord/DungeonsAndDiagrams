7fcd17fe6f32710e9a9dfe4576470050a31bdcf9
kord <davekordalewski@gmail.com>
2023-01-20 20:22:56 -0500

    progress

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# New Puzzle Toolbox
+# Dungeons and Diagrams
 
-## Generating Grids
-
+This is an original puzzle by [Zachtronics](https://en.wikipedia.org/wiki/Zachtronics)
+from the game [Last Call BBS](https://www.zachtronics.com/last-call-bbs/).
+I dug it so I recreated it for a laugh.
+                      
+                      
 ## Links
 
-- [Graphology API](https://graphology.github.io/)
-  
+- [Graphology API](https://graphology.github.io/) I didn't end up using this.
+- [Reddit](https://www.reddit.com/r/lastcallbbs/) Maybe post this here when done.
--- src/components/rulesPopup.js
+++ src/components/rulesPopup.js
@@ -6,44 +6,37 @@ class RulesPopup extends React.Component {
         super(props);
     }
 
-    render() {
 
-        var myBigGreenDialog = {
+    // t = `The terrifying labyrinth has ${rulesList.length} rules.`
+
+    render() {
+        const myBigGreenDialog = {
             backgroundColor: '#77C76E',
             color: '#ffffff',
             width: '70%',
-            height: '80%',
-            marginTop: '-35%',
+            height: '70%',
+            marginTop: '-18%',
             marginLeft: '-35%',
         };
 
+        const rulesList = [
+            'Every spot in our terrifying labyrinth is either a floor or a wall.',
+            'All of the floor space in the labyrinth is connected.',
+            'Every dead end has a monster in it.',
+            'Treasure chests are in a 3x3 room with only one exit.',
+            'Nowhere outside a treasure room is there a 2x2 floor area, the corridors are tight.',
+        ];
         return (
           <>
               <button className={'button game-button__rules'} onClick={() => this.customDialog.show()}>Rules</button>
               <SkyLight dialogStyles={myBigGreenDialog} hideOnOverlayClicked ref={ref => this.customDialog = ref}
-                        title="Kuromasu Rules">
+                        title="Dungeons and Diagrams Rules">
                   <ul>
-                      <li>
-                          Every tile needs to be red or blue.
-                      </li>
-                      <li>
-                          Numbers indicate how many blues a tile needs to be able to see.
-                      </li>
-                      <li>
-                          A blue tile is said to "see" another blue tile if the (horizontal/vertical only) path to it
-                          contains no red tiles.
-                      </li>
-                      <li>
-                          The blue tiles are all connected. Moving between adjacent blue tiles, you can visit them all.
-                          More technically, they form a single connected component.
-                      </li>
-                      <li>
-                          Click around making tiles red and blue until you have a configuration that is consistent with
-                          the provided information. I promise there is only 1 such configuration for a given initial
-                          setup.
-                      </li>
+                      {rulesList.map(r => <li>{r}</li>)}
                       <br/>
-                      Check out <a href='https://0hn0.com/'>0h n0</a> for a quick tutorial with nice animation.
+                      This is an original puzzle by <a href={'https://en.wikipedia.org/wiki/Zachtronics'}>
+                      Zachtronics</a> from the game <a href={'https://www.zachtronics.com/last-call-bbs/'}>
+                      Last Call BBS</a>.
                   </ul>
               </SkyLight>
           </>
