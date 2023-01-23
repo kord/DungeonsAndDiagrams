891b3c7bef796047d9a5a41f1521e05220f42016
kord <davekordalewski@gmail.com>
2023-01-23 10:59:46 -0500

    get rid of some unused code

--- src/components/rules.tsx
+++ src/components/rules.tsx
@@ -4,12 +4,12 @@ import '../css/rulesModal.css';
 
 const rulesList = [
     'Every spot in the terrifying labyrinth is either a floor or a wall.',
+    'Rows and columns list the number of walls they contain. Put all of the walls in the right place to win.',
     'All of the floor space in the labyrinth is connected.',
     'Every dead end has a monster in it, every monster is in a dead end.',
-    'Treasure chests are in a 3x3 room with only one exit.',
+    `Treasure chests are in a 3x3 room with only one exit. They can be located in any of the room's 9 floor spaces`,
     'Nowhere outside a treasure room is there a 2x2 floor area, the corridors are tight.',
-    'Rows and columns have the number of walls they contain listed. Put all of the walls in the right place to win.',
-    'Mark walls with left click, floors with right click, and press ctrl-z to undo.',
+    `Mark walls with left click, floors with right click, and press ctrl-z to undo. Sorry if you're on a touch device.`,
 ];
 
 
